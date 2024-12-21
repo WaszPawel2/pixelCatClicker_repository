54f7b2471a0bd260e911f0cf12bbfcbbf3f1edd0
Paweł <playbosshd.pl@gmail.com>
2024-12-21 17:12:43 +0100

    git checkout -b test_main_branch

--- app/src/main/java/com/example/pcc/MainActivity.java
+++ app/src/main/java/com/example/pcc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pcc;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView money_ammount_text;
+
+    private ImageView cat_id;
+
+    private int p_money_ammount = 0;
+
+    private int p_money_multiplier = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        money_ammount_text = findViewById(R.id.money_ammount_text);
+
+        cat_id = findViewById(R.id.cat_id);
+        cat_id.setOnClickListener(v -> addMoney());
+    }
+    private void addMoney(){
+        p_money_ammount++;
+        money_ammount_text.setText(String.valueOf(p_money_ammount));
     }
 }
